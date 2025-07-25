67e958c0af04766bd190f2b59b2dc6b6b3296029
Andi Mataraja <amataraja@gmail.com>
2025-07-26 01:58:19 +0700

    Deleting firebase config

--- src/app/assignment/11/2540124324/firebase/config.js
+++ src/app/assignment/11/2540124324/firebase/config.js
@@ -8,12 +8,7 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApGyhS1EQ0pN9kZOf-YJH-uP55FlNb1BY",
-  authDomain: "fe-bootcamp-week9.firebaseapp.com",
-  projectId: "fe-bootcamp-week9",
-  storageBucket: "fe-bootcamp-week9.firebasestorage.app",
-  messagingSenderId: "614215837920",
-  appId: "1:614215837920:web:ecd43a6428d23bf9350885",
+
 };
 
 const app = initializeApp(firebaseConfig);
